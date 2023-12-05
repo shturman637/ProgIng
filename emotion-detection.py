e48eb4b4246437ac0ed485b850e82d9b8298adc7
Sergey <152925976+shturman637@users.noreply.github.com>
2023-12-05 22:58:37 +0300

    Update emotion-detection.py

--- emotion-detection.py
+++ emotion-detection.py
@@ -3,3 +3,4 @@ from transformers import pipeline
 classifier  = pipeline("sentiment-analysis", "cointegrated/rubert-tiny2-cedr-emotion-detection")
 
 classifier("Праздник к нам приходит")
+## модель показывает эмоциональную окраску текста

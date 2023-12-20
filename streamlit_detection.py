d493d799a212d5abba353dae08b3891ea2295553
Sergey <152925976+shturman637@users.noreply.github.com>
2023-12-20 19:36:06 +0300

    Update streamlit_detection.py

--- streamlit_detection.py
+++ streamlit_detection.py
@@ -23,21 +23,3 @@ if st.button("Анализировать текст"):
 
 # Устанавливаем фон 
 
-def get_base64(bin_file):
-    with open(bin_file, 'rb') as f:
-        data = f.read()
-    return base64.b64encode(data).decode()
-
-def set_background(png_file):
-    bin_str = get_base64(png_file)
-    page_bg_img = '''
-    <style>
-    .stApp {
-    background-image: url("data:image/png;base64,%s");
-    background-size: cover;
-    }
-    </style>
-    ''' % bin_str
-    st.markdown(page_bg_img, unsafe_allow_html=True)
-
-set_background('./images/file5.png')
